347d247d3ac8298925800802332aa864972f5842
Marco Portacio <marcoportacio4@gmail.com>
2024-06-14 13:49:13 -0500

    Fixed android crash and implemented persistent desktop database

--- composeApp/src/desktopMain/kotlin/dashboard/data/DatabaseDriver.desktop.kt
+++ composeApp/src/desktopMain/kotlin/dashboard/data/DatabaseDriver.desktop.kt
@@ -3,12 +3,24 @@ package dashboard.data
 import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.jdbc.sqlite.JdbcSqliteDriver
 import com.programmingmasters.lambda.cache.LambdaDatabase
+import java.io.File
 
 class DesktopDatabaseDriverFactory: DatabaseDriverFactory {
     override fun createDriver(): SqlDriver {
-        //TODO: Change this behavior to  a persistent database
-        val driver = JdbcSqliteDriver(JdbcSqliteDriver.IN_MEMORY)
-        LambdaDatabase.Schema.create(driver)
+        val parentFolder = File(System.getProperty("user.home") + "/LambdaAdminPanel")
+
+        if (!parentFolder.exists()) {
+            parentFolder.mkdirs()
+        }
+
+        val databasePath = File(parentFolder, "LambdaDatabase.db")
+        val driver = JdbcSqliteDriver(url = "jdbc:sqlite:${databasePath.absolutePath}")
+        
+        if (!databasePath.exists() || databasePath.length() == 0L) {
+            LambdaDatabase.Schema.create(driver)
+        }
+
         return driver
+
     }
 }
